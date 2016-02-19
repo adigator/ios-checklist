4324f1e05876dabef9bed357d898a9c26b4b7f00
iteo <iteo@Mac-mini-iteo.local>
2016-02-19 15:25:23 +0100

    checklist is editable

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
     var items: [ChecklistItem]
     required init?(coder aDecoder: NSCoder) {
         items = [ChecklistItem]()
@@ -71,10 +71,11 @@ class ChecklistViewController: UITableViewController {
     
     func configureCheckmarkForCell(cell: UITableViewCell,
         withChecklistItem item: ChecklistItem) {
+        let label = cell.viewWithTag(1001) as! UILabel
         if item.checked {
-            cell.accessoryType = .Checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .None
+            label.text = ""
         }
     }
     func configureTextForCell(
@@ -84,17 +85,37 @@ class ChecklistViewController: UITableViewController {
             label.text = item.text
     }
     
-    @IBAction func addItem() {
-            let newRowIndex = items.count
-            let item = ChecklistItem()
-            item.text = "I am new row"
-            item.checked = false
-            items.append(item)
-            let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
-            let indexPaths = [indexPath]
-            tableView.insertRowsAtIndexPaths(indexPaths,
-                                withRowAnimation: .Automatic)
+    
+    func addItemViewControllerDidCancel(controller: AddItemViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    func addItemViewController(controller: AddItemViewController, didFinishAddingItem item: ChecklistItem) {
+                let newRowIndex = items.count
+                items.append(item)
+                let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+                let indexPaths = [indexPath]
+                tableView.insertRowsAtIndexPaths(indexPaths,
+                withRowAnimation: .Automatic)
+                dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+                if segue.identifier == "AddItem" {
+                    let navigationController = segue.destinationViewController as! UINavigationController
+                    let controller = navigationController.topViewController as! AddItemViewController
+                    controller.delegate = self
+            } else if segue.identifier == "EditItem" {
+                let navigationController = segue.destinationViewController as! UINavigationController
+                let controller = navigationController.topViewController as! AddItemViewController
+                controller.delegate = self
+                if let indexPath = tableView.indexPathForCell(sender as! UITableViewCell) {
+                    controller.itemToEdit = items[indexPath.row]
+                }
+        
+        }
+    }
+ 
     
    }
 
